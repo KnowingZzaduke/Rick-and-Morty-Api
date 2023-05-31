15cd27b1df49c2542495cee2d8e4611f81ee3fb8
KnowingZzaduke <josex.developer@gmail.com>
2023-05-30 20:24:30 -0500

    Agregando detalles finales

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import {
   FaRegArrowAltCircleRight,
   FaRegArrowAltCircleLeft,
 } from "react-icons/fa";
-
+import rickAndMorty from "/img/rick-and-morty.png";
 function App() {
   const [data, setData] = useState(null);
   const [count, setCount] = useState(1);
   const [name, setName] = useState("");
   const [loader, setLoader] = useState(true);
+  const [results, setResults] = useState(false);
   const cardVariants = {
     offscreen: {
       y: 200,
@@ -30,42 +31,59 @@ function App() {
   };
 
   useEffect(() => {
+    const getData = localStorage.getItem("data");
+    if (getData) {
+      const parseData = JSON.parse(getData);
+      setData(parseData);
+    }
+  }, [count]);
+
+  useEffect(() => {
     const axiosData = async () => {
       try {
         const response = await axios.get(
-          `https://rickandmortyapi.com/api/character/?page${count}`
+          `https://rickandmortyapi.com/api/character/?page=${count}`
         );
         setData(response.data.results);
-        setLoader(false)
+        setLoader(false);
       } catch (error) {
         console.log(error);
       }
     };
     axiosData();
-  }, [count]);
+  }, [count, name]);
   //Guardar los datos en el caché
   const saveCache = useMemo(() => data, [data]);
 
-  //Buscador
-  const filterData = useCallback((e) => {
-    e.preventDefault();
-    const filteredData = saveCache.filter((save) =>
-      save.name.toLowerCase().includes(name.toLowerCase())
-    );
-    if (filteredData) {
-      setData(filteredData);
-      setLoader(false);
-    }
-    [name, saveCache];
-  });
-  console.log(loader)
+  //Guardar los datos en el local Storage
+  useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(data));
+  }, [count]);
 
-  function backPage() {
-    setCount((prevCount) => (prevCount <= 1 ? 1 : prevCount - 1));
-  }
+  //Buscador
+  const filterData = useCallback(
+    (e) => {
+      e.preventDefault();
+      const filteredData = saveCache.filter((save) =>
+        save.name.toLowerCase().includes(name.toLowerCase())
+      );
+      if (filteredData.length === 0) {
+        setResults(true);
+      } else {
+        setData(filteredData);
+        setLoader(false);
+        setResults(false);
+      }
+    },
+    [name, saveCache]
+  );
 
   function nextPage() {
     setCount(count + 1);
+  }
+
+  function backPage() {
+    setCount((prevCount) => (prevCount <= 1 ? 1 : prevCount - 1));
   }
 
   function status(status) {
@@ -88,6 +106,7 @@ function App() {
     <div className="App">
       <header>
         <h1>Rick and Morty API</h1>
+        <img src={rickAndMorty} />
         <form className="content_search" onSubmit={filterData}>
           <div className="input">
             <label htmlFor="search">Search</label>
@@ -105,6 +124,11 @@ function App() {
           <FaRegArrowAltCircleRight onClick={nextPage} />
         </div>
       </header>
+      {results && (
+        <div className="results">
+          <h2>¡ No results :( !</h2>
+        </div>
+      )}
       <div className="content_cards">
         {saveCache?.map((date) => (
           <motion.div
@@ -127,13 +151,13 @@ function App() {
         ))}
       </div>
       {loader && <div className="loader"></div>}
-
       <div className="content_buttons">
         <FaRegArrowAltCircleLeft onClick={backPage} />
         <FaRegArrowAltCircleRight onClick={nextPage} />
       </div>
       <footer>
         <p>José Luis Arteta Buelvas❤️</p>
+        <img src={rickAndMorty} />
       </footer>
     </div>
   );
